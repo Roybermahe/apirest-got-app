be98f0ce7862e4516fb323b2a4523de993d0067e
Jeffrey <jalejandromaestre@unicesar.edu.co>
2020-02-07 16:36:51 -0500

    Nuevo console.log
    se agrego una nueva linea de console.log para verificar los cambios que se realizan.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('ESCUCHANDO EN PUERTO 3000');
+  console.log('ESCUCHANDO EN PUERTO 5500');
   await app.listen(3000);
 }
 
